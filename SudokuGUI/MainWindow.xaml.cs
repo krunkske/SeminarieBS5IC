556c05db4a24fe5e3959aa8eeb705e62f556831a
krunkske <123201489+krunkske@users.noreply.github.com>
2024-04-15 15:21:43 +0200

    sr

--- SudokuGUI/MainWindow.xaml.cs
+++ SudokuGUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,10 +18,10 @@ namespace SudokuGUI
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        sudokuSolver solver = new sudokuSolver();
         TextBox[,] sudokuWaardes = new TextBox[9, 9];
         int[,] sudoku =
-{
+        {
             { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
             { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
             { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
@@ -29,40 +31,54 @@ namespace SudokuGUI
             { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
             { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
             { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
-};
+        };
+        bool[,] prePlaced;
 
         public MainWindow()
         {
             InitializeComponent();
 
 
-            for (int rij  = 0; rij < 9; rij++)
+            for (int rij = 0; rij < 9; rij++)
             {
-                for (int kol =  0; kol < 9; kol++)
+                for (int kol = 0; kol < 9; kol++)
                 {
                     TextBox sudokuWaarde = new TextBox();
                     sudokuWaardes[rij, kol] = sudokuWaarde;
-                    sudokuWaarde.Text = Convert.ToString(sudoku[rij,kol]);
+                    if (sudoku[rij, kol] != 0)
+                    {
+                        sudokuWaarde.Text = Convert.ToString(sudoku[rij, kol]);
+                        sudokuWaarde.Focusable = false;
+                    }
+                    else
+                    {
+                        sudokuWaarde.Text = "";
+                    }
+                    
                     sudokuWaarde.HorizontalContentAlignment = HorizontalAlignment.Center;
                     sudokuWaarde.VerticalContentAlignment = VerticalAlignment.Center;
                     sudokuWaarde.BorderBrush = Brushes.Black;
+                    
                     sudokuWaarde.BorderThickness = new Thickness(0.5, 0.5, 0.5, 0.5);
-                    
-                    if (rij%3 == 0)
+
+                    sudokuWaarde.LostFocus += TextBox_LostFocus;
+                    sudokuWaarde.KeyDown += TextBox_KeyDown;
+
+                    if (rij % 3 == 0)
                     {
-                        sudokuWaarde.BorderThickness = new Thickness(0.5,2,0.5,0.5);
+                        sudokuWaarde.BorderThickness = new Thickness(0.5, 2, 0.5, 0.5);
                     }
-                    
+
                     else if (rij == 8)
                     {
                         sudokuWaarde.BorderThickness = new Thickness(0.5, 0.5, 0.5, 2);
                     }
-                    
-                    if (kol%3 == 0)
+
+                    if (kol % 3 == 0)
                     {
                         if (rij % 3 == 0)
                         {
-                            sudokuWaarde.BorderThickness = new Thickness(2, 2, 0.5, 0.5); 
+                            sudokuWaarde.BorderThickness = new Thickness(2, 2, 0.5, 0.5);
                         }
                         else if (rij == 8)
                         {
@@ -72,12 +88,12 @@ namespace SudokuGUI
                         {
                             sudokuWaarde.BorderThickness = new Thickness(2, 0.5, 0.5, 0.5);
                         }
-                        
+
                     }
-                    
+
                     else if (kol == 8)
                     {
-                        if (rij%3 == 0)
+                        if (rij % 3 == 0)
                         {
                             sudokuWaarde.BorderThickness = new Thickness(0.5, 2, 2, 0.5);
                         }
@@ -89,11 +105,11 @@ namespace SudokuGUI
                         {
                             sudokuWaarde.BorderThickness = new Thickness(0.5, 0.5, 2, 0.5);
                         }
-                        
-                    }
-                    
 
-                    
+                    }
+
+
+
                     Grid.SetRow(sudokuWaarde, rij);
                     Grid.SetColumn(sudokuWaarde, kol);
 
@@ -101,19 +117,61 @@ namespace SudokuGUI
                 }
             }
 
-
-
-
-
-
             //int rijnummer = Grid.GetRow(Knop1);
             //RowDefinition rowDef = MainGrid.RowDefinitions[rijnummer];
 
         }
 
-        private void Knop1_Click(object sender, RoutedEventArgs e)
+        private void vulIn(TextBox box)
         {
+            if (box != null)
+            {
+                int rij = Grid.GetRow(box);
+                int kol = Grid.GetColumn(box);
+                if (box.Text.Length == 1 && box.Text[0] > '0' && box.Text[0] <= '9')
+                {
+                    int waarde = Convert.ToInt32(box.Text);
 
+                    if (solver.isAllesGeldig(rij, kol, waarde, sudoku))
+                    {
+                        sudoku[rij, kol] = waarde;
+                    }
+                    else
+                    {
+                        if (sudoku[rij, kol] != 0)
+                        {
+                            box.Text = Convert.ToString(sudoku[rij, kol]);
+                        }
+                        else
+                        {
+                            box.Text = "";
+                        }
+
+                    }
+                }
+                else
+                {
+                    if (sudoku[rij, kol] != 0)
+                    {
+                        box.Text = Convert.ToString(sudoku[rij, kol]);
+                    }
+                    else
+                    {
+                        box.Text = "";
+                    }
+                }
+            }
+        }
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            vulIn((TextBox)sender);
+        }
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                vulIn((TextBox)sender);
+            }
         }
     }
 }
